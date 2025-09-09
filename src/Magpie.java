1a7a6a11ed12664dbd984c66b653fed7b5bb9949
Willy-Scotty-Wav <scottw@gilmour.org>
2025-09-09 09:14:31 -0400

    magpie chatbot

--- src/Magpie.java
+++ src/Magpie.java
@@ -1,5 +1,92 @@
-public class Magpie {
-    /**
+/**
+ * A program to carry on conversations with a human user.
+ * This is the initial version that:  
+ * <ul><li>
+
+ *       Uses indexOf to find strings
+ * </li><li>
+ * 		    Handles responding to simple words and phrases 
+ * </li></ul>
+ * This version uses a nested if to handle default responses.
+ * @author Laurie White
+ * @version April 2012
+ */
+public class Magpie
+{
+	/**
+	 * Get a default greeting 	
+	 * @return a greeting
+	 */
+	public String getGreeting()
+	{
+		return "Hello, im your Mr. A chat bot.";
+	}
+	
+	/**
+	 * Gives a response to a user statement
+	 * 
+	 * @param statement
+	 *            the user statement
+	 * @return a response based on the rules given
+	 */
+	public String getResponse(String statement)
+	{
+		String response = "";
+		if (findKeyword(statement, "no") >= 0)
+		{
+			response = "Why so negative?";
+		}
+		// FAMILY
+		else if (findKeyword(statement,"mother") >= 0
+				|| findKeyword(statement,"father") >= 0
+				|| findKeyword(statement,"sister") >= 0
+				|| findKeyword(statement,"brother") >= 0)
+		{
+			response = "Tell me more about your family.";
+		}
+		// PETS
+		else if (findKeyword(statement,"dog") >= 0
+				|| findKeyword(statement,"cat") >= 0
+				|| findKeyword(statement,"bird") >= 0
+				|| findKeyword(statement,"fish") >= 0)
+				{
+					response = "tell me more about your pets";
+				}
+
+				// MR A
+		else if (findKeyword(statement,"Adiletta") != -1
+		|| findKeyword(statement,"Mr.A") != -1)
+		{
+			response = "He sounds like a cool guy";
+		} 
+
+		// I LIKE something
+		else if(findKeyword(statement, "I like") >= 0){
+			//indentify where "I like" is
+			int pos = findKeyword(statement, "I like");
+			//cut substring of thing you like
+			String thing = statement.substring(pos + 6).trim();
+
+			response = "Why do you like " + thing + "?";
+		}
+		// I WANT something
+		else if(findKeyword(statement, "I want")!= -1){
+		//locate i want
+		int pos = findKeyword(statement, "I want");
+		//chop off the after stuff
+		String thing = statement.substring(pos + 6).trim();
+
+		response = "Would you realy be happy if you had " + thing + "?";
+		}
+		else
+				{
+			response = getRandomResponse();
+		}
+		return response;
+		
+
+	}
+	/**
  * Search for one word in phrase. The search is not case
  * sensitive. This method will check that the given goal
  * is not a substring of a longer string (so, for
@@ -78,77 +165,36 @@ private int findKeyword(String statement, String goal)
 	return findKeyword(statement, goal, 0);
 }
 
-public char[] getGreeting() {
-    // TODO Auto-generated method stub
-    throw new UnsupportedOperationException("Unimplemented method 'getGreeting'");
-}
 
-public char[] getResponse(String statement) {
-   if(statement.trim().length() == 0){
-        response = "Please say something.";
-   }
-    /**
-	 * Take a statement with "I want to <something>." and transform it into 
-	 * "What would it mean to <something>?"
-	 * @param statement the user statement, assumed to contain "I want to"
-	 * @return the transformed statement
-	 */
-	private String transformIWantToStatement(String statement)
-	{
-		//  Remove the final period, if there is one
-		statement = statement.trim();
-		String lastChar = statement.substring(statement
-				.length() - 1);
-		if (lastChar.equals("."))
-		{
-			statement = statement.substring(0, statement
-					.length() - 1);
-		}
-		int psn = findKeyword (statement, "I want to", 0);
-		String restOfStatement = statement.substring(psn + 9).trim();
-		return "What would it mean to " + restOfStatement + "?";
-	}
-
-	
 	
 	/**
-	 * Take a statement with "you <something> me" and transform it into 
-	 * "What makes you think that I <something> you?"
-	 * @param statement the user statement, assumed to contain "you" followed by "me"
-	 * @return the transformed statement
+	 * Pick a default response to use if nothing else fits.
+	 * @return a non-committal string
 	 */
-	private String transformYouMeStatement(String statement)
+	private String getRandomResponse()
 	{
-		//  Remove the final period, if there is one
-		statement = statement.trim();
-		String lastChar = statement.substring(statement
-				.length() - 1);
-		if (lastChar.equals("."))
+		final int NUMBER_OF_RESPONSES = 4;
+		double r = Math.random();
+		int whichResponse = (int)(r * NUMBER_OF_RESPONSES);
+		String response = "";
+		
+		if (whichResponse == 0)
 		{
-			statement = statement.substring(0, statement
-					.length() - 1);
+			response = "Interesting, tell me more.";
+		}
+		else if (whichResponse == 1)
+		{
+			response = "Hmmm.";
+		}
+		else if (whichResponse == 2)
+		{
+			response = "Do you really think so?";
+		}
+		else if (whichResponse == 3)
+		{
+			response = "You don't say.";
 		}
 		
-		int psnOfYou = findKeyword (statement, "you", 0);
-		int psnOfMe = findKeyword (statement, "me", psnOfYou + 3);
-		
-		String restOfStatement = statement.substring(psnOfYou + 3, psnOfMe).trim();
-		return "What makes you think that I " + restOfStatement + " you?";
+		return response;
 	}
 }
-}
-}
-if (statement.indexOf("no") > = 0)
-response = "why so negative?";
-
-if (statement.indexOf("family") > = 0)
-
-if (statement.indexOf("Mother") != -1)
-if (statement.indexOf("Father") <= 0)
-if (statement.indexOf("Brother") <= 0)
-if (statement.indexOf("Sister") <= 0)
-
-response = "Tell me more about your family";
-
-
-else if(statement.indexOf(Addeletta))
