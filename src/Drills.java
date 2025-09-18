35c6fcfd434c32a43de4566a50f3313e0160c31f
Willy-Scotty-Wav <scottw@gilmour.org>
2025-09-18 09:21:03 -0400

    drilllsssss
    Co-authored-by: Dan Adiletta <dadiletta@users.noreply.github.com>

--- src/Drills.java
+++ src/Drills.java
@@ -1,4 +1,6 @@
-    public class Drills {
+import java.util.Scanner;
+
+public class Drills {
     
     public static void main(String[] args) {
     
@@ -26,21 +28,45 @@
         for(int num: testScores) total += num;
         System.out.println("Avrage: " + (total/testScores.length));
         // Count how many scores are above 85 using a loop
-        for(int i = 0; i > 85; i ++){
-            System.out.println(testScores[i]);
+        int count = 0;
+        for(int num : testScores){
+
+            if (num > 85) count ++;
         }
+        System.out.println(count);
         // An infinite loop that collects user input
-        
+        Scanner s = new Scanner (System.in);
+        while(mark){
+            System.out.println("Enter a Number: ");
+            int input = s.nextInt();
+   
             // A short-circuit conditional with four tests
             // (check if input is: positive && less than 100 && even && divisible by 5)
-        
+            if( input > 0 && input < 100 && input % 2 == 0 && input % 5 == 0){
+                System.out.println("Passed");
                 // Break the loop if all conditions pass
         
+                break;
+            }
+        }     
         // Loop through each char in the String "DataScience"
-        // Print each character on a separate line with its index
-                
+        String aBetterName = "DataScience";
+        for (int i = 0; i < aBetterName.length(); i++){
+            // Print each character on a separate line with its index
+            System.out.print(aBetterName.charAt(i) + " - ");
+
+        }       
         // Given String word = "Algorithm", print only the first three letters
-        
+        String Marcus = "Algorithm";
+        System.out.println(Marcus.substring(0,3)); 
+            // Print each character on a separate line with its index
+            for (int i = 0; i < Marcus.length(); i++){
+            System.out.print(Marcus.charAt(i) + " - "); 
+        }
+        int odd = 0;
+        for(int i = 0; i < 100; i ++){
+            if(1 % 2 !=0 ) System.out.println(" " + i);
+        }
         // Print all the odd numbers from 1-100 in rows of 10 numbers each
         
         // Create a Scanner and take a String input (ask for user's name)
--- src/MagpieRunner.java
+++ src/MagpieRunner.java
@@ -1,4 +1,4 @@
-  import java.util.Scanner;
+ // import java.util.Scanner;
 
 
 /**
@@ -6,26 +6,26 @@
  * @author Laurie White
  * @version April 2012
  */
-public class MagpieRunner
-{
+//public class MagpieRunner
+//{
 	/**
 	 * Create a Magpie, give it user input, and print its replies.
 	 */
-	public static void main(String[] args)
-	{
-		Magpie maggie = new Magpie();
+	//public static void main(String[] args)
+	//{
+		//Magpie maggie = new Magpie();
 		
-		System.out.println (maggie.getGreeting());
-		Scanner in = new Scanner (System.in);
-		String statement = in.nextLine();
+		//System.out.println (maggie.getGreeting());
+		//Scanner in = new Scanner (System.in);
+		//String statement = in.nextLine();
 		
-		while (!statement.equals("Bye"))
-		{
-			System.out.println (maggie.getResponse(statement));
-			statement = in.nextLine();
-		}
-	}
+		//while (!statement.equals("Bye"))
+		//{
+			//System.out.println (maggie.getResponse(statement));
+			//statement = in.nextLine();
+		//}
+	//}
 	
-}
+//}
 
 
